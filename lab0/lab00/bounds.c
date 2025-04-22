d0c0c964ec6d283f8906e87812230dd68dd99fde
AgU131 <agu.heredia.urbinatti@gmail.com>
2025-04-22 13:50:07 -0300

    lab0 done

--- lab0/lab00/bounds.c
+++ lab0/lab00/bounds.c
@@ -24,20 +24,20 @@ struct bound_data check_bound(int value, int arr[], unsigned int length) {
     // Comprobar si el valor es un upperbound
     for (unsigned int i = 0; i < length; i++) {
         is_upper = is_upper && (arr[i] <= value);
-        // if (arr[i] >= value) {
+        // if (arr[i] <= value) {
         //     is_upper = true;
         // }
         
         // Comprobar si el valor es un lowerbound
         is_lower = is_lower && (arr[i] >= value);
-        // if (arr[i] <= value) {
+        // if (arr[i] >= value) {
         //     is_lower = true;
         // }
 
         // Comprobar si el valor existe en el array y dar el indice donde esta
         exists = exists || (arr[i] == value);
         if (arr[i] == value) {
-            res.exists = true;
+            // res.exists = true;
             res.where = i;
         }
     }
@@ -50,28 +50,30 @@ struct bound_data check_bound(int value, int arr[], unsigned int length) {
 }
 
 int main(void) {
-    int a[ARRAY_SIZE] = {0, -1, 9, 4};
-    int value=9;
+    // Variables originales
+    // int a[ARRAY_SIZE] = {0, -1, 9, 4};
+    // int value=9;
     // TODO: COMPLETAR - Pedir entrada al usuario
-    // int i = 0;
-    // int b[ARRAY_SIZE], valor;
-    // while (i < ARRAY_SIZE)
-    // {
-    //     printf("ingrese el valor %i del arreglo:\n");
-    //     scanf("%d", &a[i]);
-    //     i++;
-    // }
-    // printf("ingrese el valor:\n");
-    // scanf("%d", &valor);
+
+    int a[ARRAY_SIZE], value;
+    int i = 0;
+    while (i < ARRAY_SIZE)
+    {
+        printf("ingrese el valor %i del arreglo:\n", i);
+        scanf("%d", &a[i]);
+        i++;
+    }
+    printf("ingrese el valor:\n");
+    scanf("%d", &value);
 
 
     struct bound_data result = check_bound(value, a, ARRAY_SIZE);
 
     // TODO: REEMPLAZAR LO SIGUIENTE LUEGO POR LA SALIDA QUE PIDE EL ENUNCIADO
-    printf("is_upperbound = %d\n", result.is_upperbound); // Imprime 1
-    printf("is_lower = %d\n", result.is_lowerbound); // Imprime 0
-    printf("exists = %u\n", result.exists);        // Imprime 1
-    printf("where = %u\n", result.where);         // Imprime 2
+    printf("cota superior? (is_upperbound) = %d\n", result.is_upperbound); // Imprime 1
+    printf("cota inferior? (is_lower) = %d\n", result.is_lowerbound); // Imprime 0
+    printf("existe? = %u\n", result.exists);        // Imprime 1
+    printf("where? = %u\n", result.where);         // Imprime 2
 
     return EXIT_SUCCESS;
 }
