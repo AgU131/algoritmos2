57a8a9206176e4ecbfb4ef31d37a780465908b84
AgU131 <agu.heredia.urbinatti@gmail.com>
2025-04-22 18:17:03 -0300

    lab1

--- lab1/lab01/ej0/fixstring.c
+++ lab1/lab01/ej0/fixstring.c
@@ -20,10 +20,11 @@ unsigned int fstring_length(fixstring s) {
 }
 
 bool fstring_eq(fixstring s1, fixstring s2) {
-    int i = 0;
+    unsigned int i = 0;
     bool son_iguales = true;
     unsigned int tam1 = fstring_length(s1);
-    while (i < tam1) {
+    while (i < tam1 && son_iguales) {
+
         if (s1[i] != s2[i]) {
             son_iguales = false;
         }
@@ -41,10 +42,11 @@ bool fstring_less_eq(fixstring s1, fixstring s2) {
     unsigned int tam1 = fstring_length(s1);
     unsigned int tam2 = fstring_length(s2);
     
-    while (s1[i] == s2[i] && s1[i] == "\0") {
+    while ((s1[i] == s2[i]) && (s1[i] != '\0')) {
         i++;
     }
-    return ((s1[i] < s2[i]) || (tam1 < tam2));
+
+    return (s1[i] <= s2[i]);
 } 
 
 // Aca tenia el error de ir comparando por caracter a caracter y si habia alguno que era menor o ig que el otro me daba true,
@@ -54,15 +56,15 @@ bool fstring_less_eq(fixstring s1, fixstring s2) {
 // FALSE esta mal ESTE ERROR
 
 
-int main() {
-    fixstring a = "alessss";
-    fixstring b = "halesss";
+// int main() {
+//     fixstring a = "alessss";
+//     fixstring b = "halesss";
 
-    printf("tamano: %u \n", fstring_length(a));
-    printf("equivalente: %d\n"
-        "[1 = true y 0 = false]\n",
-        fstring_eq(a, b));
-    printf("s1 es menor o igual: %d \n", fstring_less_eq(a, b));
+//     printf("tamano: %u \n", fstring_length(a));
+//     printf("equivalente: %d\n"
+//         "[1 = true y 0 = false]\n",
+//         fstring_eq(a, b));
+//     printf("s1 es menor o igual: %d \n", fstring_less_eq(a, b));
         
-    return 0;
-}
+//     return 0;
+// }
