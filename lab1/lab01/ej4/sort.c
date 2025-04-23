1f2719ba1dbcd6d2e783004c7219866c7fb3a83b
AgU131 <agu.heredia.urbinatti@gmail.com>
2025-04-23 18:39:25 -0300

    ej4

--- lab1/lab01/ej4/sort.c
+++ lab1/lab01/ej4/sort.c
@@ -27,21 +27,55 @@ void selection_sort(int a[], unsigned int length) {
 
 static void insert(int a[], unsigned int i) {
     /* copiá acá la implementación que hiciste en el ejercicio 1 */
+    unsigned int j = i;
+    while (j > 0 && goes_before(a[j], a[j - 1])) {
+        swap(a, j, j - 1);
+        j--;
+    }
 }
 
 void insertion_sort(int a[], unsigned int length) {
     /* copiá acá la implementación que hiciste en el ejercicio 1 */
+    for (unsigned int i = 1; i < length; ++i) {
+        insert(a, i);
+    }
+    assert(array_is_sorted(a, length));
 }
 
 
 static unsigned int partition(int a[], unsigned int izq, unsigned int der) {
     /* copiá acá la implementación que hiciste en el ejercicio 3 */
+    int i = izq+1, j = der;
+    unsigned int ppiv = izq;
+    while (i <= j) {
+        if (a[i] <= a[ppiv]) 
+        {
+            i++;
+        } else if (a[j] >= a[ppiv]) 
+        {
+            j--;
+        } else if (goes_before(a[ppiv], a[i]) && (goes_before(a[j], a[ppiv])))
+        {
+            swap(a,i,j);
+        }
+    }
+    swap(a,ppiv,j);
+    ppiv = j;
+
+    return ppiv;
 }
 
 static void quick_sort_rec(int a[], unsigned int izq, unsigned int der) {
     /* copiá acá la implementación que hiciste en el ejercicio 2 */
+    unsigned int ppiv;
+    if (der > izq) {
+        ppiv = partition(a,izq,der);
+        quick_sort_rec(a,izq,ppiv-1);
+        quick_sort_rec(a,ppiv+1,der);
+    }
 }
 
 void quick_sort(int a[], unsigned int length) {
     /* copiá acá la implementación que hiciste en el ejercicio 1 */
+    quick_sort_rec(a, 0, (length == 0) ? 0 : length - 1);
 }
