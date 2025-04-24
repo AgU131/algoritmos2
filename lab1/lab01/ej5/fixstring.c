26dd4a6c3a99eada3747ff58add71b4cb5ede131
AgU131 <agu.heredia.urbinatti@gmail.com>
2025-04-23 21:12:56 -0300

    fixstring implementations done

--- lab1/lab01/ej5/fixstring.c
+++ lab1/lab01/ej5/fixstring.c
@@ -6,14 +6,32 @@
 
 unsigned int fstring_length(fixstring s) {
     /* copiá acá la implementación que hiciste en el ejercicio 0 */
+    int i = 0;
+    while (s[i] != '\0') {
+        i++;
+    }
+    
+    return i;
 }
 
 bool fstring_eq(fixstring s1, fixstring s2) {
     /* copiá acá la implementación que hiciste en el ejercicio 0 */
+    int i = 0;
+    while ((s1[i] == s2[i]) && (s1[i] != '\0')) {
+        i++;
+    }
+
+    return (s1[i] == s2[i]);
 }
 
 bool fstring_less_eq(fixstring s1, fixstring s2) {
     /* copiá acá la implementación que hiciste en el ejercicio 0 */
+    int i = 0;
+    while ((s1[i] == s2[i]) && (s1[i] != '\0')) {
+        i++;
+    }
+
+    return (s1[i] <= s2[i]);
 }
 
 void fstring_set(fixstring s1, const fixstring s2) {
@@ -27,10 +45,10 @@ void fstring_set(fixstring s1, const fixstring s2) {
 
 void fstring_swap(fixstring s1,  fixstring s2) {
     fixstring aux;
-    /*
-     * COMPLETAR
-     *
-     */
+    /*COMPLETAR*/
+    fstring_set(aux, s1);
+    fstring_set(s1, s2);
+    fstring_set(s2, aux);
 }
 
 
