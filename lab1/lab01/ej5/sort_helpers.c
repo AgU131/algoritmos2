083f07d7e97ae2a8a59f609e7b480ad67073feda
AgU131 <agu.heredia.urbinatti@gmail.com>
2025-04-24 00:33:12 -0300

    corrections ej5

--- lab1/lab01/ej5/sort_helpers.c
+++ lab1/lab01/ej5/sort_helpers.c
@@ -9,12 +9,12 @@
 
 void swap(fixstring a[], unsigned int i, unsigned int j) {
     /*COMPLETAR*/
-    fixstring_swap(a[i], a[j]);
+    fstring_swap(a[i], a[j]);
 }
 
 bool goes_before(fixstring x, fixstring y) {
     /*COMPLETAR*/
-    return fixstring_less_eq(x, y);
+    return (fstring_less_eq(x, y));
 }
 
 bool array_is_sorted(fixstring array[], unsigned int length) {
