21835f110b40f1d35f9f9efbe9b14f6597c45524
Agustin Heredia <agustin.heredia.urbinatti@mi.unc.edu.ar>
2025-05-29 09:50:26 -0300

    abs2 done

--- lab4/lab04/ej2/abs2.c
+++ lab4/lab04/ej2/abs2.c
@@ -1,19 +1,25 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <assert.h>
 
 void absolute(int x, int *y) {
-    //
-    // Completar aquí
-    //
+    *y = x;
+    if (*y < 0) {
+        *y = -x;  // Si x es negativo, asigna -x a y
+    }
 }
 
 int main(void) {
-    int a=0, res=0;  // No modificar esta declaración
+    int a=-100, res=0;  // No modificar esta declaración
     // --- No se deben declarar variables nuevas ---
 
-    //
-    // Completar aquí
-    //
+    printf("res antes de absolute = %d\n", res);
+    printf("a = %d\n", a);
+
+    absolute(a, &res);
+
+    printf("res = %d\n", res);
+
     assert(res >= 0 && (res == a || res == -a));
     return EXIT_SUCCESS;
 }
