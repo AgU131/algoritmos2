a76e56f4ed7a4a03143cf3b52d10ee1cd8971548
AgU131 <agu.heredia.urbinatti@gmail.com>
2025-05-29 11:01:40 -0300

    swap done

--- lab4/lab04/ej2/swap.c
+++ lab4/lab04/ej2/swap.c
@@ -1,3 +1,37 @@
-//
-// Completar aquí
-//
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+/*
+Ejercicio Traducir:
+proc swap(in/out a : int, in/out b : int) 
+    var aux : int 
+    aux := a 
+    a := b 
+    b := aux  
+end proc 
+fun main() ret r : int 
+  var x, y : int 
+  x := 6 
+  y := 4 
+  print(x, y) 
+  swap(x, y) 
+  print(x, y) 
+  r := 0 
+end fun
+*/
+
+void swap (int *a, int *b){
+    int aux;
+    aux = *a;
+    *a = *b;
+    *b = aux;
+}
+int main(void) {
+    int x=6, y=4;
+    printf("\nx = %d,\ny = %d\n", x, y);   //Variables antes del swap
+    swap(&x, &y);
+    printf("\nx = %d,\ny = %d\n", x, y);   //Variables despues del swap
+    
+    return EXIT_SUCCESS;
+}
