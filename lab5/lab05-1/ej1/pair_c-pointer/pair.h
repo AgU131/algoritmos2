8a81ba6a2bfc9e6170a034e26d0b16552c821c9c
Agustin <82238717+AgU131@users.noreply.github.com>
2025-05-30 15:49:08 -0300

    Notas explicacion encapsulacion added

--- lab5/lab05-1/ej1/pair_c-pointer/pair.h
+++ lab5/lab05-1/ej1/pair_c-pointer/pair.h
@@ -2,7 +2,8 @@
 #define _PAIR_H
 
 typedef struct s_pair_t * pair_t;
-
+// El encapsulamiento es intentar lo menor posible de codigo oalgoi asi. Es que solo con la implementacion los demas puedan leer y entender el codigo sin saber como esta especificado.
+// POrlo tanto lo que esta debajo rompe esa encapsulacion, no es necesario agregar la definicion de s_pair_t, sino que puedo colocarlo directamente en el ".c"
 struct s_pair_t {
     int fst;
     int snd;
