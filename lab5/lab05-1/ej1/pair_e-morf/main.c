0395836a8f12861f0ce54edb239a817e91d5f56d
Agustin Heredia <agustin.heredia.urbinatti@mi.unc.edu.ar>
2025-06-02 13:41:52 -0300

    ejercicio e) done

--- lab5/lab05-1/ej1/pair_e-morf/main.c
+++ lab5/lab05-1/ej1/pair_e-morf/main.c
@@ -32,5 +32,32 @@ int main(void) {
     p = pair_destroy(p);
     q = pair_destroy(q);
 
+
+    
+    //Nuvo ejemplo de Polimorfirsmo:
+    pair_t r, s;
+    // Nuevo par p
+    r = pair_new('a', 'x');
+
+    // Se muestra el par por pantalla
+    printf("r = ");
+    show_pair(r);
+
+    // Nuevo para q con elementos de p intercambiados
+    s = pair_swapped(r);
+
+    // Se vuelve a mostrar p
+    printf("r = ");
+    show_pair(r);
+
+    // Se muestra s
+    printf("s = ");
+    show_pair(s);
+
+    // Se destruyen r y s
+    r = pair_destroy(r);
+    s = pair_destroy(s);
+
+
     return EXIT_SUCCESS;
 }
--- lab5/lab05-1/ej1/pair_e-morf/pair.c
+++ lab5/lab05-1/ej1/pair_e-morf/pair.c
@@ -3,22 +3,22 @@
 #include "pair.h"    /* TAD Par         */
 
 struct s_pair_t {
-    int fst;
-    int snd;
+    elem fst;
+    elem snd;
 };
 
-pair_t pair_new(int x, int y) {
-    pair_t p = malloc(sizeof(struct s_pair_t));
+pair_t pair_new(elem x, elem y) {
+    pair_t p = malloc(sizeof(pair_t));
     p->fst = x;
     p->snd = y;
     return p;
 }
 
-int pair_first(pair_t p) {
+elem pair_first(pair_t p) {
     return (p->fst);
 }    
 
-int pair_second(pair_t p)  {
+elem pair_second(pair_t p)  {
     return (p->snd);
 }    
 
