a440b3bc02b80e394051a9e0bef50e298d8f7fe3
Agustin Heredia <agustin.heredia.urbinatti@mi.unc.edu.ar>
2025-06-10 17:36:21 -0300

    fixing sizes done

--- lab4/lab04/ej3/a-sizes/sizes.c
+++ lab4/lab04/ej3/a-sizes/sizes.c
@@ -20,10 +20,11 @@ int main(void) {
     printf("name-size  : %lu bytes\n"
            "age-size   : %lu bytes\n"
            "height-size: %lu bytes\n"
-           "data_t-size: %lu bytes\n",  sizeof(messi.name), 
-                                        sizeof(messi.age),
-                                        sizeof(messi.height),
-                                        sizeof(messi.name) + sizeof(messi.age) + sizeof(messi.height));
+           "data_t-size: %lu bytes\n",  
+           sizeof(messi.name), 
+           sizeof(messi.age),
+           sizeof(messi.height),
+           sizeof(messi.name) + sizeof(messi.age) + sizeof(messi.height));
 
     /*     COMPLETAR     */
 
@@ -31,7 +32,7 @@ int main(void) {
     unsigned *p = NULL;
     data_t *q = NULL;
     
-    printf("Direcciones de Memoria:");
+    printf("\nDirecciones de Memoria:\n");
     r = &messi.name;
     printf("Direccion de name-size: %p\n", (void *) r);
     p = &messi.age;
